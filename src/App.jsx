2d722fd64b78b0af9098b91683849d1c2fa20f19
Juan Santacruz <juanjojhon2005@gmail.com>
2025-02-19 14:46:23 -0500

    challenge 2 [vite-proyect]

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+const FirstApp = (props)=>{
+  return (
+    <div>
+    <h1>  {props.title}</h1>
+    <span>10</span>
+     
+    </div>
+    
+  )
+}
+export default FirstApp
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+
+import './index.css'
+import FirstApp from './App.jsx'
+import  React   from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FirstApp title = "Hoy gana el madrid  "/>
+  </React.StrictMode>
+)
