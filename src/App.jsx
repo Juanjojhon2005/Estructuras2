87a89ba1a83c57e2563f1b14be8ca2591a4c6202
Juan Santacruz <juanjojhon2005@gmail.com>
2025-04-03 21:56:47 -0500

    Challenge-2Estesies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react';
+import FirstApp from './componentes/FirstApp';
+
+const App = () => {
+    return (
+        <div>
+            <FirstApp />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/componentes/FirstApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FirstApp = () => {
+    const title = 'First App';
+    const number = 10;
+
+    return (
+        <>
+            <h1>{title}</h1>
+            <span>{number}</span>
+        </>
+    );
+};
+
+export default FirstApp;
